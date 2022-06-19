420773974d3e7435bbece1524afe8a569ea1f09c
brkp <brkp@tutanota.com>
2022-06-19 14:09:31 +0300

    `write_chunk` implementation

--- src/chunk.c
+++ src/chunk.c
@@ -11,4 +11,12 @@ void free_chunk(Chunk *chunk) {
     FREE_ARRAY(u8, chunk->code, chunk->cap);
 }
 
-void write_chunk(Chunk *chunk, u8 byte) {}
+void write_chunk(Chunk *chunk, u8 byte) {
+    if (chunk->len + 1 >= chunk->cap) {
+        int old_cap = chunk->cap;
+        chunk->cap  = GROW_CAPACITY(old_cap);
+        chunk->code = GROW_ARRAY(u8, chunk->code, old_cap, chunk->cap);
+    }
+
+    chunk->code[chunk->len++] = byte;
+}
