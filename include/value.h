f4096695bc460b68b1cc4555ba594f33a89659d1
brkp <brkp@tutanota.com>
2022-06-19 22:22:14 +0300

    definitions to work with the `Value` type

---
+++ include/value.h
@@ -0,0 +1,18 @@
+#ifndef clox_value_h
+#define clox_value_h
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+    int len;
+    int cap;
+    Value *values;
+} ValueArray;
+
+void value_array_init(ValueArray *array);
+void value_array_free(ValueArray *array);
+void value_array_push(ValueArray *array, Value value);
+
+#endif
