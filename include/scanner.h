ea0728598b53cf37acfeb512fb3c9e5b9f3c4d5f
brkp <brkp@tutanota.com>
2022-06-25 11:40:50 +0300

    `scanner_init` function

--- include/scanner.h
+++ include/scanner.h
@@ -3,4 +3,12 @@
 
 #include "common.h"
 
+typedef struct {
+    const char *start;
+    const char *current;
+    int line;
+} Scanner;
+
+void scanner_init(Scanner *scanner, const char *source);
+
 #endif
--- src/scanner.c
+++ src/scanner.c
@@ -1,1 +1,7 @@
 #include "scanner.h"
+
+void scanner_init(Scanner *scanner, const char *source) {
+    scanner->line = 0;
+    scanner->start = source;
+    scanner->current = source;
+}
