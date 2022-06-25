6a62392fe59f6602131112b69a74d97212b41d39
brkp <brkp@tutanota.com>
2022-06-25 11:57:04 +0300

    `Token` struct and `TokenType` enum

--- include/scanner.h
+++ include/scanner.h
@@ -3,11 +3,39 @@
 
 #include "common.h"
 
+typedef enum {
+  TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
+  TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
+  TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
+  TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
+
+  TOKEN_BANG, TOKEN_BANG_EQUAL,
+  TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
+  TOKEN_GREATER, TOKEN_GREATER_EQUAL,
+  TOKEN_LESS, TOKEN_LESS_EQUAL,
+
+  TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+
+  TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
+  TOKEN_FOR, TOKEN_FN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
+  TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
+  TOKEN_TRUE, TOKEN_LET, TOKEN_WHILE,
+
+  TOKEN_ERROR, TOKEN_EOF
+} TokenType;
+
 typedef struct {
     const char *start;
     const char *current;
     int line;
 } Scanner;
+
+typedef struct {
+    int line;
+    int length;
+    TokenType type;
+    const char *start;
+} Token;
 
 void scanner_init(Scanner *scanner, const char *source);
 
