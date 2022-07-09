ab136ba0b73ddfc5f2bf1516572b3b28cb9c37c7
fxxf <fxxf@tutanota.com>
2022-07-10 00:44:20 +0300

    basic parser functions and the `Precedence` enum

--- src/compiler.c
+++ src/compiler.c
@@ -12,7 +12,14 @@ typedef struct {
     bool panic_mode;
 
     Chunk *compiling_chunk;
+    Scanner scanner;
 } Parser;
+
+static void parser_init(Parser *parser, Chunk *chunk) {
+    parser->had_error = false;
+    parser->panic_mode = false;
+    parser->compiling_chunk = chunk;
+}
 
 static void error_at(Parser *parser, Token *token, const char *message) {
     if (parser->panic_mode)
@@ -39,20 +46,20 @@ static void error_at_current(Parser *parser, const char *message) {
     error_at(parser, &parser->curr, message);
 }
 
-static void advance(Scanner *scanner, Parser *parser) {
+static void advance(Parser *parser) {
     parser->prev = parser->curr;
 
     for (;;) {
-        parser->curr = scanner_scan_token(scanner);
+        parser->curr = scanner_scan_token(&parser->scanner);
         if (parser->curr.type != TOKEN_ERROR) break;
 
         error_at_current(parser, parser->curr.start);
     }
 }
 
-static void consume(Scanner *scanner, Parser *parser, TokenType type, const char *message) {
+static void consume(Parser *parser, TokenType type, const char *message) {
     if (parser->curr.type == type) {
-        advance(scanner, parser);
+        advance(parser);
         return;
     }
 
@@ -67,17 +74,40 @@ static void emit_return(Parser *parser) {
     emit_byte(parser, OP_RETURN);
 }
 
+static void expression(Parser *parser) {}
+
+static void grouping(Parser *parser) {
+    expression(parser);
+    consume(parser, TOKEN_RIGHT_PAREN, "Expect '(' after expression.");
+}
+
+static void number(Parser *parser) {
+    chunk_push_constant(
+        parser->compiling_chunk,
+        strtod(parser->prev.start, NULL), parser->prev.line);
+}
+
+static void unary(Parser *parser) {
+    TokenType operator = parser->prev.type;
+    expression(parser);
+
+    switch (operator) {
+        case TOKEN_MINUS:
+            emit_byte(parser, OP_NEGATE);
+            break;
+        default:
+            return;
+    }
+}
+
 bool compile(const char *source, Chunk *chunk) {
-    Scanner scanner;
-    scanner_init(&scanner, source);
-
     Parser parser;
-    parser.had_error = false;
-    parser.panic_mode = false;
-    parser.compiling_chunk = chunk;
+    parser_init(&parser, chunk);
+    scanner_init(&parser.scanner, source);
 
-    advance(&scanner, &parser);
-    consume(&scanner, &parser, TOKEN_EOF, "Expect end of expression.");
+    advance(&parser);
+    expression(&parser);
+    consume(&parser, TOKEN_EOF, "Expect end of expression.");
 
     emit_return(&parser);
 
