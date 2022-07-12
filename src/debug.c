b368a9d385648b17c2e900fd9d56bb820c5ac594
brkp <brkp@tutanota.com>
2022-07-12 22:03:49 +0300

    op_[nil, true, false]

--- src/debug.c
+++ src/debug.c
@@ -51,6 +51,12 @@ int disassemble_opcode(Chunk *chunk, int offset) {
             return constant_opcode("OP_CONSTANT", chunk, offset);
         case OP_CONSTANT_LONG:
             return constant_long_opcode("OP_CONSTANT_LONG", chunk, offset);
+        case OP_NIL:
+            return simple_opcode("OP_NIL", offset);
+        case OP_TRUE:
+            return simple_opcode("OP_TRUE", offset);
+        case OP_FALSE:
+            return simple_opcode("OP_FALSE", offset);
         case OP_ADD:
             return simple_opcode("OP_ADD", offset);
         case OP_SUBTRACT:
