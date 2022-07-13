d34f2409bdc5f00b71e3fa0d876da75e35a4c8aa
brkp <brkp@tutanota.com>
2022-07-13 17:51:45 +0300

    utils module

---
+++ include/utils.h
@@ -0,0 +1,6 @@
+#ifndef clox_utils_h
+#define clox_utils_h
+
+char *read_file(const char *path);
+
+#endif
--- src/main.c
+++ src/main.c
@@ -6,36 +6,7 @@
 #include "chunk.h"
 #include "debug.h"
 #include "vm.h"
-
-static char *read_file(const char *path) {
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
-        fprintf(stderr, "couldn't open file '%s'\n", path);
-        return NULL;
-    }
-
-    fseek(file, 0L, SEEK_END);
-    size_t file_size = ftell(file);
-    rewind(file);
-
-    char *buffer = malloc(file_size + 1);
-    if (buffer == NULL) {
-        fprintf(stderr, "not enough memory to read '%s'\n", path);
-        fclose(file);
-        return NULL;
-    }
-
-    size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
-    if (bytes_read < file_size) {
-        fprintf(stderr, "couldn't read file '%s'\n", path);
-        fclose(file); free(buffer);
-        return NULL;
-    }
-    buffer[bytes_read] = '\0';
-
-    fclose(file);
-    return buffer;
-}
+#include "utils.h"
 
 static int repl(void) {
     return 0;
